66797eda2d0fcaada7d41a52e5e0b64e90d0adaa
Christian Lamb <christianlamb@MacBook-Pro-52.local>
2018-10-17 12:18:25 -0500

    Initial Commit

---
+++ iOS assesment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS assesment
+//
+//  Created by Christian Lamb on 10/17/18.
+//  Copyright © 2018 Christian Lamb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
